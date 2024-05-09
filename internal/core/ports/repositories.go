d57c0c989a5983101b14418e81ecdffabee989d1
Shahid Yousuf <shahidyousuf77@gmail.com>
2024-05-09 18:13:18 +0530

    - add ports - interface definitions used to communicate with actors

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -1,1 +1,8 @@
 package ports
+
+import "gosweeper/internal/core/domain"
+
+type GameRepository interface {
+	Get(id string) (domain.Game, error)
+	Save(game domain.Game) error
+}
--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -1,1 +1,9 @@
 package ports
+
+import "gosweeper/internal/core/domain"
+
+type GameService interface {
+	Get(id string) (domain.Game, error)
+	Create(name string, size uint, bombs uint) (domain.Game, error)
+	Reveal(id string, row uint, col uint) (domain.Game, error)
+}
